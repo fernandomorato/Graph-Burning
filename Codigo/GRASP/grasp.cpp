0e6e28d33661585bce184c83bb5397ac349d77e9
Fernando <fernandomorato93@gmail.com>
2022-10-18 17:28:14 -0300

    Organizando a entrada e saida

--- Codigo/GRASP/grasp.cpp
+++ Codigo/GRASP/grasp.cpp
@@ -7,12 +7,11 @@ mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count());
 vector<pair<int, vector<int>>> solutions; // {valor, lista}
 
 const int MAXN = 2e5 + 5;
-const double ALFA = 0.5;
 clock_t timer;
 
 // OUTPUT
-int densidade, numero_de_vertices, numero_de_arestas, grau_maximo, grau_medio, numero_de_iteracoes, iteracoes_solucao;
-double tempo_total, tempo_solucao;
+int densidade, numero_de_vertices, numero_de_arestas, grau_maximo, numero_de_iteracoes, iteracoes_solucao;
+double grau_medio, tempo_total, tempo_solucao;
 
 struct Node {
 	vector<Node*> vizinhos;
@@ -39,8 +38,6 @@ Node *vertice[MAXN];
 vector<int> adj[MAXN];
 int grau[MAXN];
 
-inline int f(vector<int> &sol) { return (int) sol.size(); }
-
 inline int g(int v) { return vertice[v]->grau; }
 
 int escolhe(set<int> &st) {
@@ -64,13 +61,7 @@ int escolhe(set<int> &st) {
 	return cara;
 }
 
-void printa(set<int> &st) {
-	for (auto x : st)
-		printf("%d ", x + 1);
-	putchar('\n');
-}
-
-pair<vector<int>, bool> greedy() {
+pair<vector<int>, bool> greedy(double ALPHA = 0.5) {
 	set<int> safe; // vertices que estao safe
 	set<int> quase; // vertices que estao quase queimados
 	set<int> burned; // vertices que estao queimados
@@ -109,20 +100,20 @@ pair<vector<int>, bool> greedy() {
 	return make_pair(sol, true);
 }
 
-tuple<int, double, vector<int>> GRASP() {
+tuple<int, double, vector<int>> GRASP(int maximo_iteracoes, double ALPHA) {
 	int best_f = (int) 1e9;
 	int iteracao = -1;
 	double tempo = 0;
 	vector<int> best_sol;
-	for (int i = 0; i < 1000; i++) {
-		auto ret = greedy();
+	for (int i = 0; i < maximo_iteracoes; i++) {
+		auto ret = greedy(ALPHA);
 		if (!ret.second) continue;
 		vector<int> sol = ret.first;
 		// assert(valid(sol));
 		// if (!valid(sol))
 		// 	ajeita(sol);
 		// sol = busca_local(sol);
-		int f_sol = f(sol);
+		int f_sol = (int) sol.size();
 		if (f_sol < best_f) {
 			tempo = 1.0 * (clock() - timer) / CLOCKS_PER_SEC;
 			iteracao = i + 1;
@@ -137,6 +128,9 @@ tuple<int, double, vector<int>> GRASP() {
 void readInput() {
 	set<int> st;
 	cin >> n >> m;
+	densidade = 2.0 * m / (n * (n - 1));
+	numero_de_vertices = n;
+	numero_de_arestas = m;
 	for (int i = 0; i < m; i++) {
 		int a, b;
 		cin >> a >> b;
@@ -153,16 +147,28 @@ void readInput() {
 		vertice[b]->addEdge(vertice[a]);
 		grau[a]++;
 		grau[b]++;
+		grau_maximo = max({grau_maximo, grau[a], grau[b]});
 	}
 	assert(*st.begin() >= 0 && *st.rbegin() <= n - 1);
 }
 
 int main(int argc, char **argv) {
-	// ios::sync_with_stdio(false);
-	// cin.tie();
+	freopen(argv[1].c_str(), "r", stdin);	// argv[1] = Arquivo de entrada
+	int seed = atoi(argv[2]);				// argv[2] = Seed
+	double ALPHA = atof(argv[3]);			// argv[3] = Valor de ALPHA
+	int maximo_iteracoes = atoi(argv[4]);	// argv[4] = Numero maximo de iteracoes permitida
+	int condicao_parada = atoi(argv[5]);	// argv[5] = Condicao de parada utilizada (nao usado no momento)
+	int criterio_guloso = atoi(argv[6]);	// argv[6] = Funcao gulosa utilizada
+
 	readInput();
+
+	for (int i = 0; i < n; i++) {
+		grau_medio += vertice[i]->grau;
+	}
+	grau_medio /= n;
+
 	timer = clock();
-	auto v = GRASP();
+	auto v = GRASP(maximo_iteracoes, ALPHA);
 	int iteracao = get<0>(v);
 	double tempoParaSolucao = get<1>(v);
 	int bn = (int) get<2>(v).size();
