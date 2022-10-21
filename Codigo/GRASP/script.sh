c0d5235de92148967ee072f4cb33495617fff9f0
Fernando <fernandomorato93@gmail.com>
2022-10-21 15:35:30 -0300

    Melhor solucao pro c-fat500-10 :)

--- Codigo/GRASP/script.sh
+++ Codigo/GRASP/script.sh
@@ -1,32 +1,16 @@
+filename="../../Datasets/input/c-fat500-10_edges.txt"
+seed=42
+alpha=0.7
+max_iter=1000
+criterio_parada=0
+funcao_gulosa=0
+
 make grasp
-for filename in ../../Datasets/parsed-datasets/m*edges.txt ; do
+# for filename in ../../Datasets/input/ree*edges.txt ; do
 	a=$(basename $filename .txt)
 	b=$(head -n 1 "$filename")
-	./grasp $filename 42 0.5 1000 0 0
+	./grasp $filename $seed $alpha $max_iter $criterio_parada $funcao_gulosa
 	echo $a $b
-done
+# done
 rm grasp
-exit
-# make gen
-# ./gen > random.in
-# make grasp
-# ./grasp $(basename random .in) $(head -n 1 random.in) < random.in > random.out
-# return
-# seed=42
-# nIteracoes=1000
-# criterioParada=1
-# funcaoGulosa=1
-
-make grasp
-for filename in ../../Datasets/parsed-datasets/c-*edges.txt ; do
-	k=$(basename $filename .txt)
-	b=$(head -n 1 "$filename")
-	echo Rodando $k
-	./grasp $k $b < "$filename" > ../../Datasets/parsed-datasets/$k.out
-done
-
-make combine
-for filename in ../../Datasets/parsed-datasets/c-*.out ; do
-	k=$(basename $filename .out)
-	./combine $k < "$filename"
-done
+# exit
