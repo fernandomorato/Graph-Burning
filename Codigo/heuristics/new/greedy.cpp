0d5e97bc10fc5115c163cedcb16140903589e7fb
Fernando Morato <fernandomorato93@gmail.com>
2023-02-13 15:22:49 -0300

    Commiting for safety

--- Codigo/heuristics/new/greedy.cpp
+++ Codigo/heuristics/new/greedy.cpp
@@ -13,7 +13,7 @@ double GRAPH_DENSITY, ALPHA;
 int BURNING_NUMBER;
 vector<double> centrality_scores;
 int m_adj[MAXN][MAXN], label[MAXN];
-bool blocked[MAXN];
+bool blocked[MAXN], visited[MAXN];
 
 void readInput() {
 	cin >> N >> M;
@@ -51,11 +51,61 @@ void bfs(int source) {
 }
 
 int choose_vertex(set<int> &possible_vertices, mt19937 &rng) {
- return 0;
+	return 0;
+}
+
+set<int> dfs(int current_vertex) {
+	for (int neighbour : adj[current_vertex]) {
+
+	}
+}
+
+void calc_centrality_scores(vector<double> &centrality_scores, set<int> &vertices, mt19937 &rng) {
+	int EPS = 1e-3;
+	int tamanho = (int) vertices.size();
+	vector<double> x0(tamanho), x1(tamanho);
+	uniform_real_distribution<double> unif(0, 1);
+	double norm = 0;
+	for (int i = 0; i < tamanho; i++) {
+		x1[i] = unif(rng);
+		norm += x1[i] * x1[i];
+	}
+	norm = sqrt(norm);
+	for (int i = 0; i < tamanho; i++) {
+		x1[i] /= norm;
+	}
+	int iter = 0;
+	double lambda = 0, lambdaOld = 0, m = 0;
+	while (iter <= 100) {
+		swap(x0, x1);
+		int ptr1 = 0;
+		for (int i : vertices) {
+			x1[ptr1++] = 0;
+			int ptr2 = 0;
+			for (int j : vertices) {
+				x1[i] += (double) adj_matrix[i][j] * x0[ptr2++];
+			}
+		}
+		norm = 0;
+		for (int i = 0; i < tamanho; i++) {
+			norm += x1[i] * x1[i];
+		}
+		norm = sqrt(norm);
+		lambda = 0;
+		for (int i = 0; i < tamanho; i++) {
+			x1[i] /= norm;
+			lambda += x1[i] * x0[i];
+		}
+		lambda = sqrt(lambda);
+		iter++;
+		if (fabs((lambda - lambdaOld) / lambda) < EPS)
+			break;
+		lambdaOld = lambda;
+	}
 }
 
 vector<int> construction(vector<double> centrality, mt19937 &rng) { // vetor por referencia?
-	vector<int> vertices(N), burning_sequence;
+	vector<int> vertices(N);
 	iota(vertices.begin(), vertices.end(), 0);
 	double best_centrality_score = *max_element(centrality.begin(), centrality.end());
 	vector<int> selected_vertices;
@@ -78,38 +128,72 @@ vector<int> construction(vector<double> centrality, mt19937 &rng) { // vetor por
 	assert(!selected_vertices.empty());
 	int initial_vertex = selected_vertices[rng() % ((int) selected_vertices.size())];
 
-	return {initial_vertex};
+	// return {initial_vertex};
 
 	bfs(initial_vertex);
 	set<int> safe, targeted, burned;
 	for (int i = 0; i < N; i++) {
-		safe.insert(i);
+		if (label[i] >= BURNING_NUMBER) {
+			safe.insert(i);
+		} else {
+			blocked[i] = true;
+			if (label[i] == 1)
+				burned.insert(i);
+			else
+				targeted.insert(i);
+		}
 	}
 	int current_round = 1; // para saber quem será queimado
+	vector<int> burning_sequence;
 	burning_sequence.push_back(initial_vertex);
-	burned.insert(initial_vertex);
-	safe.erase(initial_vertex);
-	for (int i = 0; i < N; i++) {
-		if (i != initial_vertex && label[i] < BURNING_NUMBER) {
-			blocked[i] = true;
-			targeted.insert(i);
-			safe.erase(i);
-		}
-	}
-	vector<pair<double, set<int>>> connected_components; // densidade, vertices da componente
-	while (!safe.empty() || !targeted.empty()) {
-		int vertices_count = 0;
-		int edges_count = 0;
-		set<int> st;
+	current_round++; // Comecamos no round 2
+	do {
+		// Aqui, vamos produzir as componentes conexas com uma dfs
+		vector<pair<double, set<int>>> connected_components; // [i] = {densidade, vertices da componente}
 		for (int i = 0; i < N; i++) {
-			// dfs();
+			if (!blocked[i] && !visited[i]) {
+				int qtd_arestas_componente = 0;
+				set<int> vertices_componente = dfs(i, arestas);
+				int qtd_vertices_componente = (int) vertices_componente.size();
+				double densidade = 2.0 * double(qtd_arestas_componente) / (double(qtd_vertices_componente) * double(qtd_vertices_componente - 1));
+				calc_centrality_scores(centrality, vertices_componente, rng);
+				connected_components.emplace_back(densidade, vertices_componente);
+			}
 		}
-		set<int> candidate_list;
+		vector<int> cl; // Candidate List
 		for (auto &cc : connected_components) {
-
+			double densidade = cc.first;
+			set<int> vertices_componente = cc.second;
+			vector<int> cl_local;
+			for (auto current_vertex : vertices_componente) {
+				if (safe.count(current_vertex)) {
+					cl_local.push_back(current_vertex);
+				}
+			}
+			sort(cl_local.rbegin(), cl_local.rend(), [&](int &a, int &b) {
+				return centrality[a] > centrality[b];
+			});
+			for (auto current_vertex : cl_local) {
+				if (centrality[current_vertex] >= (1.0 - densidade) * best_centrality_score_local)
+					cl.push_back(current_vertex);
+			}
 		}
-		int to_burn = choose_vertex(candidate_list, rng);
-	}
+		if (cl.empty()) assert(safe.empty());
+		// for (int i = 0; i < N; i++) {
+		// 	visited[i] = false;
+		// }
+		for (int i = 0; i < N; i++) {
+			if (label[i] == current_round) {
+				safe.erase(i);
+				targeted.erase(i);
+				burned.insert(i);
+				blocked[i] = true;
+			} else if (label[i] == current_round + 1) {
+				safe.erase(i);
+				targeted.insert(i);
+			}
+		}
+	} while ((int) burned.size() != N);
 	return burning_sequence;
 }
 
