76c277074027d86be655900d9831b2c298fce2dc
Fernando <fernandomorato93@gmail.com>
2023-02-18 03:30:18 +0000

    First real tests

--- scripts/script_gr_reactive.sh
+++ scripts/script_gr_reactive.sh
@@ -1,5 +1,5 @@
 for filename in instances/*; do
 	echo $filename
-	g++ -o executables/gr_reactive code/gr_reactive.cpp && ./executables/gr_reactive -seed 42 -alpha 0.5 -tl 5 -ip "$filename" -op output/output_gr_reactive.csv -lp logs/log_gr_reactive.log -ap alpha_results.csv
+	g++ -o executables/gr_reactive code/gr_reactive.cpp && ./executables/gr_reactive -seed 42 -alpha 0.5 -tl 600 -ip "$filename" -op output/output_gr_reactive.csv -lp logs/log_gr_reactive.log -ap alpha_results.csv
 done
-# python3 graphics.py
+python3 graphics.py
