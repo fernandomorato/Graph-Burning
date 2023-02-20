dfb6cf72e74b5b20f6b1d8b8b4ce62bbe5cb4df6
Fernando <fernandomorato93@gmail.com>
2023-02-19 22:46:35 -0300

    Update scripts

--- scripts/script_gr.sh
+++ scripts/script_gr.sh
@@ -1,4 +1,5 @@
+g++ -o executables/gr code/gr.cpp
 for filename in instances/*; do
-	echo $filename
-	g++ -o executables/gr code/gr.cpp && ./executables/gr -seed 42 -alpha 0.5 -tl 600 -ip "$filename" -op output/output_gr.csv -lp logs/log_gr.log
+	# echo $filename
+	 ./executables/gr -seed 42 -alpha 0.51 -tl 600 -ip "$filename" -op output/output_gr.csv -lp logs/log_gr.log &
 done
