bce7592711229f91a9bf10d8155e8b374d32efb2
Fernando <fernandomorato93@gmail.com>
2023-02-20 15:11:24 +0000

    Updated gr_bias tests

--- scripts/script_gr_bias.sh
+++ scripts/script_gr_bias.sh
@@ -1,5 +1,5 @@
 g++ -o executables/gr_bias code/gr_bias.cpp
 for filename in instances/*; do
 	# echo $filename
-	./executables/gr_bias -seed 42 -alpha 0.76 -tl 600 -ip "$filename" -op output/output_gr_bias.csv -lp logs/log_gr_bias.log &
+	./executables/gr_bias -seed 42 -alpha 0.42 -tl 600 -ip "$filename" -op output/output_gr_bias.csv -lp logs/log_gr_bias.log &
 done
