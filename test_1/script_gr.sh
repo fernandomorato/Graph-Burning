6e71fefbf085e193e2be7edc05b5db6e2765541b
Fernando Morato <fernandomorato93@gmail.com>
2023-03-16 09:05:17 -0300

    Updated scripts

--- test_1/script_gr.sh
+++ test_1/script_gr.sh
@@ -1,6 +1,14 @@
 g++ -o gr gr.cpp
-for filename in *.in; do
-	echo $filename
-	a=$(basename $filename .in)
-	./gr -alpha 0.51 -tl 600 -ip "$filename" -op output/output_gr/$a.csv -lp logs/logs_gr/$a.log &
+for filename in *.in ; do
+	
+	n_processes=$(ps -U fcpereira | grep -c gr)
+	while [ $n_processes -ge 30 ]
+	do
+		sleep 10
+		n_processes=$(ps -U fcpereira | grep -c gr)
+	done
+	# a=$(basename $filename .in)
+	echo "$filename"
+	# > logs/logs_gr/log_"${filename#*/}" &
+   	./gr -alpha 0.51 -tl 600 -ip "$filename" -op output/output_gr/$a.csv -lp logs/logs_gr/$a.log &
 done
--- test_1/script_gr_bias.sh
+++ test_1/script_gr_bias.sh
@@ -1,6 +1,14 @@
-# g++ -o executables/gr_bias code/gr_bias.cpp
-for filename in *.in; do
-	# echo $filename
-	a=$(basename $filename .in)
-	./gr_bias -alpha 0.42 -tl 600 -ip "$filename" -op output/output_gr_bias/$a.csv -lp logs/logs_gr_bias/$a.log &
+g++ -o gr_bias gr_bias.cpp
+for filename in *.in ; do
+	
+	n_processes=$(ps -U fcpereira | grep -c gr_bias)
+	while [ $n_processes -ge 30 ]
+	do
+		sleep 10
+		n_processes=$(ps -U fcpereira | grep -c gr_bias)
+	done
+	# a=$(basename $filename .in)
+	echo "$filename"
+	# > logs/logs_gr/log_"${filename#*/}" &
+   	./gr_bias -alpha 0.51 -tl 600 -ip "$filename" -op output/output_gr_bias/$a.csv -lp logs/logs_gr_bias/$a.log &
 done
