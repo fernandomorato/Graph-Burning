60c507d412ce8a0bd5ea4b1a8b5a28a52df5238a
Fernando Morato <fernandomorato93@gmail.com>
2023-03-20 10:35:58 -0300

    Updated script_gr

--- test_1/script_gr.sh
+++ test_1/script_gr.sh
@@ -7,9 +7,10 @@ for filename in *.in ; do
 		sleep 10
 		n_processes=$(ps -U fcpereira | grep -c gr)
 	done
-	# a=$(basename $filename .in)
+	a=$(basename $filename .in)
 	echo "$filename"
 	# > logs/logs_gr/log_"${filename#*/}" &
    	./gr -alpha 0.51 -tl 600 -ip "$filename" -op output/output_gr/$a.csv -lp logs/logs_gr/$a.log &
 done
 echo "Finished"
+exit
