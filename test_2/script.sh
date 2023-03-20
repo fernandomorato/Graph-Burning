cfe12d2b0d9e9b6edef49602bb7099f07afc7ee8
Fernando Morato <fernandomorato93@gmail.com>
2023-03-20 14:15:31 -0300

    Created test_2 environment

---
+++ test_2/script.sh
@@ -0,0 +1,5 @@
+for filename in instances/*; do
+	# echo $filename
+	a=$(basename $filename .in)
+	./executables/gr -alpha 0.51 -tl 600 -ip "$filename" -op output/important-info.csv -lp logs/log_gr/$a.log &
+done
