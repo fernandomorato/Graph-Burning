1110dcb7c28c32fc6ed6a29df5ce9e71ab5e99a1
Fernando <fernandomorato93@gmail.com>
2023-03-27 15:13:29 +0000

    Add test_5 results

--- test_5/script.sh
+++ test_5/script.sh
@@ -1,4 +1,4 @@
-for filename in instances/*.in; do
+for filename in *.in; do
 	echo $filename
 	a=$(basename $filename .in)
 	./gr -alpha 0.51 -tl 600 -ip "$filename" -op output/all-info.csv -lp logs/$a.log &
