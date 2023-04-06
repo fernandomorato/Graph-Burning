26ef52d209c657c0e96a55d2500c1d4a03858858
Fernando <fernandomorato93@gmail.com>
2023-04-06 00:46:46 +0000

    Retested grids

--- test_5/script.sh
+++ test_5/script.sh
@@ -10,5 +10,5 @@ g++ ../code/gr.cpp -o gr
 for filename in *.in; do
 	echo $filename
 	a=$(basename $filename .in)
-	./gr -alpha 0.51 -tl 1 -ip "$filename" -op output/all-info.csv -lp logs/$a.log
+	./gr -alpha 0.51 -tl 600 -ip "$filename" -op output/grids.csv -lp logs/$a.log &
 done
