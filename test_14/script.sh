1843b0e1382122be02bcde9bf7a3a589b0281cdc
Fernando <fernandomorato93@gmail.com>
2023-04-24 00:03:04 -0300

    Added test_13 and test_14

---
+++ test_14/script.sh
@@ -0,0 +1,16 @@
+mkdir logs
+mkdir output
+mkdir solutions
+g++ ../code/testes/gr-test.cpp -o gr
+for filename in instances/*.in ; do
+	n_processes=$(ps -U fcpereira | grep -c gr)
+	while [ $n_processes -ge 30 ]
+	do
+		sleep 10
+		n_processes=$(ps -U fcpereira | grep -c gr)
+	done
+	echo "$filename"
+	# for i in $(seq 1 3) ; do
+   		./gr -alpha 0.25 -tl 600 -ip "$filename" -op output/results.csv -sp solutions/sol_"${filename#*/}" -criterio 2 >> logs/log_"${filename#*/}" &
+   	# done
+done
